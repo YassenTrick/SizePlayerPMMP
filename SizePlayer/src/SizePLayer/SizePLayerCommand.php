4611e7d5e5fd7e62fe60feeb543ed46a8cccc57f
Jack Honour <gangnam253@gmail.com>
2018-06-06 19:52:37 +0100

    Update SizePLayerCommand.php

--- SizePlayer/src/SizePLayer/SizePLayerCommand.php
+++ SizePlayer/src/SizePLayer/SizePLayerCommand.php
@@ -20,6 +20,10 @@ class SizePLayerCommand extends Command{
         if($player->hasPermission("size.command")){
             if(isset($args[0])){
                 if(is_numeric($args[0])){
+                    if($args[0] > 20){
+                      $player->sendMessage("Size cannot be bigger then 20");
+                      return true;
+                    }
                     $this->plugin->size[$player->getName()] = $args[0];
                     $player->setScale($args[0]);
                     $player->sendMessage("§1- §aYour size now is ".$args[0]."!");
@@ -34,7 +38,11 @@ class SizePLayerCommand extends Command{
                 }else{
                     $player->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
                 }
+            } else {
+              $player->sendMessage("The size must be a number");
             }
+            return true;
         }
+        $player->sendMessage("You do not have permission to do this.");
     }
 }
