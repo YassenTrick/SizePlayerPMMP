cc0a4d158f9824a21781d19e882e4467767a20f1
Jack Honour <gangnam253@gmail.com>
2018-06-06 19:06:00 +0100

    Update SizePLayer.php

--- SizePlayer/src/SizePLayer/SizePLayer.php
+++ SizePlayer/src/SizePLayer/SizePLayer.php
@@ -10,9 +10,9 @@ class SizePLayer extends PluginBase{
     
     public $size = array();
     public function onEnable(){
-        $this->getLogger()->info("Enabling plugin...");
-        $this->getLogger()->notice("SizePlayer v1 succesfully enabled!!");
-        #$this->getServer()->getCommandMap()->register("size", new Size($this));
+        $this->getLogger()->info("§aEnabling plugin...");
+        $this->getLogger()->notice("§bSizePlayer v1 succesfully enabled!!");
+        $this->getServer()->getCommandMap()->register("size", new Size($this));
     }
     
     public function respawn(PlayerRespawnEvent $event){
@@ -22,28 +22,92 @@ class SizePLayer extends PluginBase{
             $player->setScale($size);
         }
     }
+}
+class SizePLayer extends Command{
     
-    public function onCommand(CommandSender $user, Command $cmd, string $label, array $args): bool{
-        $sender = $this->getServer()->getOfflinePlayer($user->getName());
-        if($sender->hasPermission("size.command")){
+    private $plugin;
+    public function __construct($plugin){
+        $this->plugin = $plugin;
+        parent::__construct("size", "Change your size.");
+    }
+    
+    public function execute(CommandSender $player, string $label, array $args){
+        if($player->hasPermission("size.command")){
             if(isset($args[0])){
                 if(is_numeric($args[0])){
-                    $this->size[$sender->getName()] = $args[0];
-                    $sender->setScale($args[0]);
-                    $sender->sendMessage("§1- §aYour size now is ".$args[0]."!");
+                    $this->plugin->size[$player->getName()] = $args[0];
+                    $player->setScale($args[0]);
+                    $player->sendMessage("§1- §aYour size now is ".$args[0]."!");
                 }elseif($args[0] == "reset"){
-                    if(!empty($this->size[$sender->getName()])){
-                        unset($this->size[$sender->getName()]);
-                        $sender->setScale(1);
-                        $sender->sendMessage("§1- §aYour size is now normal!");
+                    if(!empty($this->plugin->size[$player->getName()])){
+                        unset($this->plugin->size[$player->getName()]);
+                        $player->setScale(1);
+                        $player->sendMessage("§1- §aYour size is now normal!");
                     }else{
-                        $sender->sendMessage("§1- §aYour size has been reset!!");
+                        $player->sendMessage("§1- §aYour size has been reset!!");
                     }
                 }else{
-                    $sender->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
+                    $player->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
                 }
             }
         }
+    }
+
         return true;
+    }
+}
+<?php
+namespace SizePLayer;
+use pocketmine\plugin\PluginBase;
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\entity\Entity;
+use pocketmine\Server;
+use pocketmine\Player;
+class SizePLayer extends PluginBase{
+    
+    public $size = array();
+    public function onEnable(){
+        $this->getLogger()->info("§aEnabling plugin...");
+        $this->getLogger()->notice("§bSizePlayer v1 succesfully enabled!!");
+        $this->getServer()->getCommandMap()->register("size", new Size($this));
+    }
+    
+    public function respawn(PlayerRespawnEvent $event){
+        $player = $event->getPlayer();
+        if(!empty($this->size[$player->getName()])){
+            $size = $this->size[$player->getName()];
+            $player->setScale($size);
+        }
+    }
+}
+class SizePLayer extends Command{
+    
+    private $plugin;
+    public function __construct($plugin){
+        $this->plugin = $plugin;
+        parent::__construct("size", "Change your size.");
+    }
+    
+    public function execute(CommandSender $player, string $label, array $args){
+        if($player->hasPermission("size.command")){
+            if(isset($args[0])){
+                if(is_numeric($args[0])){
+                    $this->plugin->size[$player->getName()] = $args[0];
+                    $player->setScale($args[0]);
+                    $player->sendMessage("§1- §aYour size now is ".$args[0]."!");
+                }elseif($args[0] == "reset"){
+                    if(!empty($this->plugin->size[$player->getName()])){
+                        unset($this->plugin->size[$player->getName()]);
+                        $player->setScale(1);
+                        $player->sendMessage("§1- §aYour size is now normal!");
+                    }else{
+                        $player->sendMessage("§1- §aYour size has been reset!!");
+                    }
+                }else{
+                    $player->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
+                }
+            }
+        }
     }
 }
