d041d2b6f6c5813801e155af9182872c6fa7b650
Zade <33807566+Zadezter@users.noreply.github.com>
2018-06-12 14:00:25 +0000

    Changed.

--- SizePlayer/src/SizePLayer/SizePLayerCommand.php
+++ SizePlayer/src/SizePLayer/SizePLayerCommand.php
@@ -12,43 +12,43 @@ use pocketmine\command\{
 use pocketmine\utils\TextFormat as TF;
 use pocketmine\entity\Entity;
   
-class SizePLayerCommand extends Command{
+class SizePLayerCommand extends Command {
     
     /** var Plugin */
     private $plugin;
   
-    public function __construct($plugin){
+    public function __construct($plugin) {
         $this->plugin = $plugin;
         parent::__construct("size", "Change your player size!");
     }
     
     public function execute(CommandSender $player, string $label, array $args){
-        if($player->hasPermission("size.command")){
-            if(isset($args[0])){
-                if(is_numeric($args[0])){
-                    if($args[0] > 20){
-                      $player->sendMessage("Size cannot be bigger then 20");
+        if($player->hasPermission("size.command")) {
+            if(isset($args[0])) {
+                if(is_numeric($args[0])) {
+                    if($args[0] > 15) {
+                      $player->sendMessage(TF::RED. "This size must bigger than §e15");
                       return true;
-                    }elseif($args[0] <= 0){
-                      $player->sendMessage("Size cannot be smaller than or eqaul to 0");
+                    }elseif($args[0] <= 0) {
+                      $player->sendMessage(TF::RED. "This size cannot be smaller than or equal to §e0");
                       return true;
                     }
                     $this->plugin->size[$player->getName()] = $args[0];
                     $player->setScale($args[0]);
-                    $player->sendMessage("§1- §aYour size now is ".TF::GOLD . $args[0]."§6!");
-                }elseif($args[0] == "reset"){
-                    if(!empty($this->plugin->size[$player->getName()])){
+                    $player->sendMessage("§8§l(§a!§8)§r §aYou have changed your size to ".TF::GOLD . $args[0]."§a!");
+                }elseif($args[0] == "reset") {
+                    if(!empty($this->plugin->size[$player->getName()])) {
                         unset($this->plugin->size[$player->getName()]);
                         $player->setScale(1);
-                        $player->sendMessage("§1- §aYour size is now normal!");
+                        $player->sendMessage("§8§l(§a!§8)§r §aYou size has back to normal!");
                     }else{
-                        $player->sendMessage("§1- §aYour size has been reset!!");
+                        $player->sendMessage("§8§l(§a!§8)§r §aYou have reseted your size!");
                     }
                 }else{
                     $player->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
                 }
             } else {
-              $player->sendMessage(TF::RED. "The size must be a valid number!");
+              $player->sendMessage(TF::RED. "The size is not a valid number!");
             }
             return true;
         }
