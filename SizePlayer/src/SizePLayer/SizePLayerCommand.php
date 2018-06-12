73f709b892856f8460d3721d17074a71262cd0f7
Zade <33807566+Zadezter@users.noreply.github.com>
2018-06-12 10:32:50 +0000

    Update SizePLayerCommand.php

--- SizePlayer/src/SizePLayer/SizePLayerCommand.php
+++ SizePlayer/src/SizePLayer/SizePLayerCommand.php
@@ -1,19 +1,25 @@
 <?php
 
+declare(strict_types=1);
 namespace SizePLayer;
 
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
+use pocketmine\{
+  Server, Player
+};
+use pocketmine\command\{
+  Command, CommandSender
+};
+use pocketmine\utils\TextFormat as TF;
 use pocketmine\entity\Entity;
-use pocketmine\Server;
-use pocketmine\Player;
   
 class SizePLayerCommand extends Command{
     
+    /** var Plugin */
     private $plugin;
+  
     public function __construct($plugin){
         $this->plugin = $plugin;
-        parent::__construct("size", "Change your size.");
+        parent::__construct("size", "Change your player size!");
     }
     
     public function execute(CommandSender $player, string $label, array $args){
@@ -29,7 +35,7 @@ class SizePLayerCommand extends Command{
                     }
                     $this->plugin->size[$player->getName()] = $args[0];
                     $player->setScale($args[0]);
-                    $player->sendMessage("§1- §aYour size now is ".$args[0]."!");
+                    $player->sendMessage("§1- §aYour size now is ".TF::GOLD . $args[0]."§6!");
                 }elseif($args[0] == "reset"){
                     if(!empty($this->plugin->size[$player->getName()])){
                         unset($this->plugin->size[$player->getName()]);
@@ -42,10 +48,10 @@ class SizePLayerCommand extends Command{
                     $player->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
                 }
             } else {
-              $player->sendMessage("The size must be a number");
+              $player->sendMessage(TF::RED. "The size must be a valid number!");
             }
             return true;
         }
-        $player->sendMessage("You do not have permission to do this.");
+        $player->sendMessage(TF::RED. "You do not have permission allowed to use size command!");
     }
 }
