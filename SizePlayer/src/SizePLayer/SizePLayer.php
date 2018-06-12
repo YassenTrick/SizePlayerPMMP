17488cd54a1895bb283e104da9c96edff2c61055
Zade <33807566+Zadezter@users.noreply.github.com>
2018-06-12 10:22:54 +0000

    Added + Changed

--- SizePlayer/src/SizePLayer/SizePLayer.php
+++ SizePlayer/src/SizePLayer/SizePLayer.php
@@ -1,22 +1,39 @@
 <?php
+
+declare(strict_types=1);
 namespace SizePLayer;
-use pocketmine\plugin\PluginBase;
-use pocketmine\command\Command;
-use pocketmine\command\CommandSender;
+
+use pocketmine\plugin\{
+    PluginBase, Plugin
+};
+use pocketmine\command\{
+    Command, CommandSender
+};
+use pocketmine\{
+    Server, Player
+};
+use pocketmine\utils\TextFormat as TF;
 use pocketmine\entity\Entity;
-use pocketmine\Server;
-use pocketmine\Player;
 use SizePLayer\SizePLayerCommand;
-class SizePLayer extends PluginBase{
+
+class SizePLayer extends PluginBase {
     
+    /** var $size */
     public $size = array();
-    public function onEnable(){
-        $this->getLogger()->info("SizePlayer v1 succesfully enabled!!");
+    
+    public function onEnable(): void{
+        $this->getLogger()->info(TF::GREEN. "SizePlayer plugin is successfully enabled!");
         $this->getServer()->getCommandMap()->register("size", new SizePLayerCommand($this));
     }
     
-    public function respawn(PlayerRespawnEvent $event){
-        $player = $event->getPlayer();
+    public function onDisable(): void{
+        $this->getLogger()->info(TF::GOLD . "SizePlayer plugin is currently disabled!");
+		$this->getServer()->shutdown();
+    }
+    
+    public function onPlayerRespawn(PlayerRespawnEvent $ev): void{
+        $player = $ev->getPlayer();
+        // SIZE EVENT \\
         if(!empty($this->size[$player->getName()])){
             $size = $this->size[$player->getName()];
             $player->setScale($size);
