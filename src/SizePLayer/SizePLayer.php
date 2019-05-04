2956f06614c862fcbb52e3d0e9c68a9d9bf6de8a
Jack Honour <gangnam253@gmail.com>
2019-05-04 17:20:19 +0100

    1.1.0 (#8)
    * Start 1.1.0
    
    A feature coming soon !
    
    * First Part
    
    * Part 2, to be tested.
    
    * Finished + Tested.
    
    @YassenTrick where you at ?

--- src/SizePLayer/SizePLayer.php
+++ src/SizePLayer/SizePLayer.php
@@ -1,38 +1,51 @@
 <?php
 declare(strict_types=1);
 namespace SizePLayer;
-use pocketmine\plugin\{
-    PluginBase, Plugin
-};
-use pocketmine\command\{
-    Command, CommandSender
-};
-use pocketmine\{
-    Server, Player
-};
-use pocketmine\utils\TextFormat as TF;
-use pocketmine\entity\Entity;
-use SizePLayer\SizePLayerCommand;
-class SizePLayer extends PluginBase {
-    
-    /** var $size */
-    public $size = array();
+
+use pocketmine\event\Listener;
+use pocketmine\plugin\PluginBase;
+
+use pocketmine\utils\Config;
+
+use pocketmine\Player;
+use pocketmine\event\player\{PlayerJoinEvent, PlayerRespawnEvent};;
+
+class SizePLayer extends PluginBase implements Listener {
     
     public function onEnable(): void{
-        $this->getLogger()->info("SizePlayer plugin is successfully enabled!");
+        //no start message in compliance with poggit rules.
         $this->getServer()->getCommandMap()->register("size", new SizePLayerCommand($this));
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
+        $this->dataFile = new Config($this->getDataFolder() . "data.yml", Config::YAML, ["version" => 1, "sizes" => []]);
+        $this->data = $this->dataFile->getAll();
     }
     
     public function onDisable(): void{
         $this->getLogger()->info("SizePlayer plugin is currently disabled!");
     }
     
+    public function saveData(){
+		$this->dataFile->setAll($this->data);
+		$this->dataFile->save();
+	}
+	
+	public function updateSize(Player $player, float $size, bool $save = true): void{
+		$this->data["sizes"][$player->getLowerCaseName()] = $size;
+		$player->setScale($size);
+		if($save === true) $this->saveData();
+	}
+    
+    public function onPlayerJoin(PlayerJoinEvent $ev): void{
+        $player = $ev->getPlayer();
+		if(!empty($this->data["sizes"][$player->getLowerCaseName()])){
+            $this->updateSize($player, (float)$this->data["sizes"][$player->getLowerCaseName()], false);
+        }
+    }
+    
     public function onPlayerRespawn(PlayerRespawnEvent $ev): void{
         $player = $ev->getPlayer();
-        // SIZE EVENT \\
-        if(!empty($this->size[$player->getName()])){
-            $size = $this->size[$player->getName()];
-            $player->setScale((float)$size);
-         }
+        if(!empty($this->data["sizes"][$player->getLowerCaseName()])){
+            $this->updateSize($player, (float)$this->data["sizes"][$player->getLowerCaseName()], false);
+        }
      }
 }
